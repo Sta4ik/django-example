5b4edcd184f49c72629b442c2e6ca03aaefc6e1c
Sta4ik <sta4ik@tutamail.com>
2026-05-29 15:32:42 +0300

    get news from db

--- kernel_app/views.py
+++ kernel_app/views.py
@@ -1,14 +1,20 @@
+import base64
 from django.shortcuts import render
 from django.http import HttpResponse
 from kernel_app.models import Article, Article_Picture
 from kernel_app.forms import New_Article
+from random import randint
 
 # Create your views here.
 def main_page(request):
     return render(request, "main.html")
 
 def news_page(request):
-    return render(request, 'news.html')
+    newsList = Article.objects.all()
+    for news in newsList:
+        if news.article_preview_image:
+            news.imageBase = base64.b64encode(news.article_preview_image).decode('utf-8')
+    return render(request, 'news.html', {'newsList': newsList})
 
 def create_article(request):
     if request.method == 'POST':
@@ -18,9 +24,9 @@ def create_article(request):
             preview = form.cleaned_data['article_preview_image'].read()
             pictures = form.cleaned_data['article_pictures'].read()
 
-            article = Article(article_id = '1', article_title = form.cleaned_data['article_title'], article_annotation = form.cleaned_data['article_annotation'], 
+            article = Article(article_id = randint(1, 1000000), article_title = form.cleaned_data['article_title'], article_annotation = form.cleaned_data['article_annotation'], 
                               article_preview_image = preview, article_text = form.cleaned_data['article_text'])
-            picture_article = Article_Picture(picture_id = '1', article_id = article, picture = pictures)
+            picture_article = Article_Picture(picture_id = randint(1, 1000000), article_id = article, picture = pictures)
 
             article.save()
             picture_article.save()
